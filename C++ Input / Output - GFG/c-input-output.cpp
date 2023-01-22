3504f502e7e3670b48cd0a19dd3ebc3b6ab5e30d
jafinsunar <101696322+jafinsunar@users.noreply.github.com>
2023-01-22 15:08:12 +0530

    Added solution - LeetHub

---
+++ C++ Input / Output - GFG/c-input-output.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+
+    int multiplication(int A, int B){
+        
+        // code here
+        int x;
+        x=(A*B);
+         return x;
+        
+    }
+   
+    
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+        cout << ob.multiplication(A,B) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
