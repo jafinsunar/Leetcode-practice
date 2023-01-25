d0cc73df8e78c794187f2d6bf4f328ac7307ac54
jafinsunar <101696322+jafinsunar@users.noreply.github.com>
2023-01-25 23:55:10 +0530

    Time: 23 ms (42.01%), Space: 5.9 MB (56.92%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        int n = x;
+        long  rev =0;
+        
+        
+        
+        while (x>0) {
+            rev =(rev *10) +(x%10);
+            
+            x=x/10;
+        }
+        if(n == rev){
+            return true;
+        }
+        else{
+            return false;
+        }
+        
+    }
+};
